cc112e6059e8a0e6f6025ef33ee0c3d9f70f63a5
Mantoo452 <156635489+Mantoo452@users.noreply.github.com>
2024-01-24 14:23:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-  IC",
+    "https://github.com/Mantoo452/Mantookumar",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
